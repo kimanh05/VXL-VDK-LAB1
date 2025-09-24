c74ab565263799195064c9307226e37ba30bcb8d
KIM_ANH\kiman <anh.nguyen2510@hcmut.edu.vn>
2025-09-25 01:59:51 +0700

    Update Exercise6

--- LAB1_EX/Core/Src/main.c
+++ LAB1_EX/Core/Src/main.c
@@ -90,9 +90,57 @@ int main(void)
   /* USER CODE END 2 */
 
   /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+
   while (1)
   {
+
+	HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, RESET);
+
+	HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, SET);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, RESET);
+
+	HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, SET);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, RESET);
+
+	HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, SET);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, RESET);
+
+	HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, SET);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, RESET);
+
+	HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, SET);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, RESET);
+
+	HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, SET);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, RESET);
+
+	HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, SET);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, RESET);
+
+	HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, SET);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, RESET);
+
+	HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, SET);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, RESET);
+
+	HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, SET);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, RESET);
+
+	HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, SET);
+	HAL_Delay(500);
+	HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, RESET);
 
     /* USER CODE END WHILE */
 
@@ -149,10 +197,16 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOA, LED_RED_Pin|LED_YELLOW_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOA, LED_1_Pin|LED_2_Pin|LED_3_Pin|LED_4_Pin
+                          |LED_5_Pin|LED_6_Pin|LED_7_Pin|LED_8_Pin
+                          |LED_9_Pin|LED_10_Pin|LED_11_Pin|LED_12_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : LED_RED_Pin LED_YELLOW_Pin */
-  GPIO_InitStruct.Pin = LED_RED_Pin|LED_YELLOW_Pin;
+  /*Configure GPIO pins : LED_1_Pin LED_2_Pin LED_3_Pin LED_4_Pin
+                           LED_5_Pin LED_6_Pin LED_7_Pin LED_8_Pin
+                           LED_9_Pin LED_10_Pin LED_11_Pin LED_12_Pin */
+  GPIO_InitStruct.Pin = LED_1_Pin|LED_2_Pin|LED_3_Pin|LED_4_Pin
+                          |LED_5_Pin|LED_6_Pin|LED_7_Pin|LED_8_Pin
+                          |LED_9_Pin|LED_10_Pin|LED_11_Pin|LED_12_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
